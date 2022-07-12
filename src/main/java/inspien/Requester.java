cea791942447cad259a9385707adeb4672936ddb
dohyun kim <64508411+DOKKU2714@users.noreply.github.com>
2022-07-12 13:04:35 +0900

    Update Requester.java
    throws IOException 추가 및 들여쓰기 수정

--- src/main/java/inspien/Requester.java
+++ src/main/java/inspien/Requester.java
@@ -11,7 +11,7 @@ public class Requester {
         this.inputData = inputData;
     }
     
-    public String sendRequest() {
+    public String sendRequest() throws IOException {
         URL url = null; // url객체를 통해 httpUrlConnection 객체를 얻음
 		HttpURLConnection httpUrlConnection = null; //http 통신을 할 수 있게 해주는 객체
 		url = new URL(this.requestURL);	//url 객체 생성
@@ -31,27 +31,26 @@ public class Requester {
 		}    
         
         String readLine = null; //json 형태의 데이터를 한 줄 씩 읽기 위한 변수
-		StringBuilder buffer = null; //readLine 변수로 한 줄씩 읽은 데이터를 저장하기위한 버퍼
-		buffer = new StringBuilder();
-		//try-catch-finally 를 통한 writer close 를 하지않고 try-with-resources 를 통해 간단하게 구현
-		try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-				httpUrlConnection.getInputStream(), "UTF-8"))) {
-			if(httpUrlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) { //응답 코드가 200일 경우
-				
-				while((readLine = bufferedReader.readLine()) != null) {
-					buffer.append(readLine)
-							.append("\n");
-				}
-			} else {
-				buffer.append("code : ")
-						.append(httpUrlConnection.getResponseCode())
-						.append("\n")
-						.append("message : ")
-						.append(httpUrlConnection.getResponseMessage())
+	StringBuilder buffer = null; //readLine 변수로 한 줄씩 읽은 데이터를 저장하기위한 버퍼
+	buffer = new StringBuilder();
+	//try-catch-finally 를 통한 writer close 를 하지않고 try-with-resources 를 통해 간단하게 구현
+	try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
+			httpUrlConnection.getInputStream(), "UTF-8"))) {
+		if(httpUrlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) { //응답 코드가 200일 경우
+
+			while((readLine = bufferedReader.readLine()) != null) {
+				buffer.append(readLine)
 						.append("\n");
 			}
+		} else {
+			buffer.append("code : ")
+					.append(httpUrlConnection.getResponseCode())
+					.append("\n")
+					.append("message : ")
+					.append(httpUrlConnection.getResponseMessage())
+					.append("\n");
 		}
-		
-		return buffer.toString();
+	}
+	return buffer.toString();
     }
 }
