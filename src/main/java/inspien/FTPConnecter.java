a54faf06b68dbf5006c80933d94f12a04823d897
dohyun kim <64508411+DOKKU2714@users.noreply.github.com>
2022-07-12 14:55:01 +0900

    Update and rename FTPConnecter.java to FTPControl.java

--- src/main/java/inspien/FTPConnecter.java
+++
@@ -1,5 +0,0 @@
-package inspien;
-
-public class FTPConnecter {
-
-}
---
+++ src/main/java/inspien/FTPControl.java
@@ -0,0 +1,8 @@
+package inspien;
+
+public class FTPControl {
+    //ftpClient 객체 생성
+    //ftp 연결 (ip 와 port 번호)
+    //ftp 인코딩 설정
+    //ftp 서버로부터 응답 코드를 받음
+}
