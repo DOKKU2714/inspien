e88f016cdcdf14d06bc43c03a550a635833dd6c8
dohyun kim <64508411+DOKKU2714@users.noreply.github.com>
2022-07-12 11:02:33 +0900

    Update Requester.java
    생성자 및 필드 선언

--- src/main/java/inspien/Requester.java
+++ src/main/java/inspien/Requester.java
@@ -1,5 +1,14 @@
 package inspien;
 
 public class Requester {
-
+    private String requestURL;
+    private String inputData;
+    private String responseData;
+  
+    Requester(){}
+    
+    Requester(String requestURL, String inputData) {
+        this.requestURL = requestURL;
+        this.inputData = inputData;
+    }
 }
